b795b531ebd27726dda010923ce88fa2e2cbf64d
Adrian Grillo <adriangrillo@USF-Gold-Wifi-226-4-83.laptops.usf.edu>
2021-02-01 16:26:59 -0500

    Init commit

--- src/components/home-components/HomePage.js
+++ src/components/home-components/HomePage.js
@@ -5,11 +5,6 @@ import RecipeRoulette from './RecipeRoulette'
 // import { handleRandomRecipes } from '../../actions/random-recipes'
 // import RandomRecipe from './RandomRecipe'
 
-// // url is a placeholder, the actual url will be whatever the endpoint is for the spoonacular API
-
-// Commented out below is the original HomePage design, trying out something different after
-// seeing mysaffronapp.com
-
 export default function HomePage() {
     return (
         <div className='container column'>
--- src/components/home-components/RecipeRoulette.js
+++ src/components/home-components/RecipeRoulette.js
@@ -1,42 +1,77 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { handleRandomRecipes } from '../../actions/random-recipes'
-
+import { handleRecipeRoulette } from '../../actions/recipe-roulette'
+import IoTimeOutline from 'react-ionicons/lib/IosTimeOutline'
+import { Transition } from 'react-transition-group'
 
 // Recipe Roulette idea: This component will display 10 cards each containing a random recipe. If the user doesn't like any of the recipes on the
-// recipes on the cards, then some animation will trigger and continue until 10 new recipes have been received from the API and can be displayed
+// cards, then some animation will trigger and continue until 10 new recipes have been received from the API and can be displayed
 // on the page.
 
 export default function RecipeRoulette() {
     const dispatch = useDispatch()
-    const randomRecipes = useSelector(state => state.randomRecipes)
+    const recipeRoulette = useSelector(state => state.recipeRoulette)
+    const url = 'https://api.spoonacular.com/recipes/random'
+
+    // Transition state + variables
+    const [inProp, setInProp] = React.useState(true)
+    const duration = 400
+    const defaultStyle = {
+        transition: `opacity ${duration}ms ease-in-out`,
+        opacity: 0
+    }
+    const transitionStyles = {
+        entering: { opacity: 1 },
+        entered: { opacity: 1 },
+        exiting: { opacity: 0 },
+        exited: { opacity: 0 }
+
+    }
 
     React.useEffect(() => {
-        dispatch(handleRandomRecipes('https://api.spoonacular.com/recipes/random', 10))
+        dispatch(handleRecipeRoulette(url, 10))
     }, [dispatch])
+
+    // After 800ms, set inProp to true so that the cards fade back onto the screen after the API request finishes
+    React.useEffect(() => {
+        const timeout = setTimeout(() => setInProp(true), 800)
+
+        return () => clearTimeout(timeout)
+    }, [inProp])
 
     return (
         <div className='center recipe-roulette'>
             <div>
                 <h1 className='title'>Recipe Roulette</h1>
-                <h2 className='title'>Nothing piquing your interest? Take it for a <button className='button'>spin!</button></h2>
+                <h2 className='title'>Nothing piquing your interest? Take it for a 
+                    <button 
+                        className='button' 
+                        onClick={() => { setInProp(false); dispatch(handleRecipeRoulette(url, 10)) }}
+                    >
+                        spin!
+                    </button>
+                </h2>
 
                 <div className='recipe-roulette-cards-container'>
-                    {randomRecipes.map((recipe, index) => (
-                        <li key={index} className='recipe-roulette-card'>
-                            {recipe.readyInMinutes}
-                        </li>
+                    {recipeRoulette.map((recipe, index) => (
+                        <Transition in={inProp} timeout={800}>
+                            {state => (
+                                <div style={{
+                                    ...defaultStyle,
+                                    ...transitionStyles[state]
+                                }}>
+                                    <li key={index} className='recipe-roulette-card'>
+                                        <img src={recipe.image} alt='finished dish' />
+                                        <p>{recipe.title}</p>
+                                        <p><IoTimeOutline /> {recipe.readyInMinutes} minutes</p>
+                                        <p>Popularity Rating: {recipe.spoonacularScore}</p>
+                                    </li>
+                                </div>
+                            )}
+                        </Transition>
                     ))}
                 </div>
             </div>
-
-            {/* <div>
-                {randomRecipes.map((recipe, index) => (
-                    <li key={index}>
-                        {recipe.vegan}
-                    </li>
-                ))}
-            </div> */}
         </div>
     )
 }
